e5b1f30432f6e582a18183be513bf54aea41129f
M <manav@xebia.com>
2019-07-09 05:13:17 +0530

    Initial Commit

---
+++ CompanyMeetingScheduler/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CompanyMeetingScheduler
+//
+//  Created by xeadmin on 09/07/19.
+//  Copyright © 2019 Manav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
